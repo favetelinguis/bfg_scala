8454d715870212863c9ed1fca4a48fdf105f87f0
Henrik Larsson <reg@henkelarsson.se>
2017-04-26 19:40:09 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,51 @@
+import Dependencies._
+
+
+lazy val commonSettings = Seq(
+  organization := "hlab.bfg",
+  version := "0.1.0-SNAPSHOT",
+  scalaVersion := "2.12.2"
+)
+
+lazy val root = (project in file("."))
+  .settings(
+    commonSettings,
+    name := "bfg6"
+  )
+  .aggregate(domain, application, infrastructure)
+
+lazy val domain = project
+  .settings(
+    commonSettings,
+    libraryDependencies ++= domainDeps
+  )
+
+lazy val application = project
+  .settings(
+    commonSettings,
+    libraryDependencies ++= applicationDeps
+  )
+  .dependsOn(domain, infrastructure)
+
+lazy val infrastructure = project
+  .settings(
+    commonSettings,
+    libraryDependencies ++= infrastructureDeps
+  )
+  .dependsOn(domain)
+
+lazy val componenttest = project
+  .settings(
+    commonSettings,
+    libraryDependencies ++= componenttestDeps
+  )
+  .dependsOn(application)
+
+lazy val loadtest = project
+  .settings(
+    commonSettings,
+    libraryDependencies ++= loadtestDeps
+  )
+  .dependsOn(application)
+
+
---
+++ project/Dependencies.scala
@@ -0,0 +1,33 @@
+import sbt._
+
+object Dependencies {
+
+  lazy val akkaVersion = "2.5.0"
+  val nd4jVersion = "0.8.0"
+
+  val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
+  val akkaStreamTestkit = "com.typesafe.akka" %% "akka-stream-testkit" % akkaVersion
+  val akkaTestkit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
+
+  val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.0.5"
+
+  val scalactic = "org.scalactic" %% "scalactic" % "3.0.1"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1"
+
+  // Not working, getting conflicting versions
+  val gatling = "io.gatling.highcharts" % "gatling-charts-highcharts" % "2.2.5"
+
+  val scalaz = "org.scalaz" %% "scalaz-core" % "7.2.11"
+
+  //Scala version dl4j not avaliable atm in alpha
+  val dl4j = "org.deeplearning4j" % "deeplearning4j-core" % "0.8.0"
+  val nd4jPlatform = "org.nd4j" % "nd4j-native-platform" % nd4jVersion
+  // Scala API for nd4j
+  val nd4j = "org.nd4j" %% "nd4s" % nd4jVersion
+
+  val domainDeps = Seq(scalaz)
+  val applicationDeps = Seq(dl4j)
+  val infrastructureDeps = Seq(akkaHttp, akkaStream)
+  val loadtestDeps = Seq()
+  val componenttestDeps = Seq(scalaTest % Test, scalactic)
+}
